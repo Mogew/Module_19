408d87e5745794db5e6b05a6eb457dcc61291988
Mogew <alimovren@gmail.com>
2023-06-11 16:35:59 +0600

    Initial Commit

---
+++ Module_19/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Module_19
+//
+//  Created by Renat on 11.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
